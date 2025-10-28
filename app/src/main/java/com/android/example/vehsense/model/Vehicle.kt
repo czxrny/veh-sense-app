0fb34fa1c9ca644c8f27dc5807a99135a3373d39
czxrny <sczarnecki091@gmail.com>
2025-10-28 19:34:12 +0100

    fix: vehicle model contains serializedname fields to correctly read the API responses

--- app/src/main/java/com/android/example/vehsense/model/Vehicle.kt
+++ app/src/main/java/com/android/example/vehsense/model/Vehicle.kt
@@ -1,15 +1,17 @@
 package com.android.example.vehsense.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Vehicle(
     val id: Int,
-    val ownerId: Int?,
-    val organizationId: Int?,
+    @SerializedName("owner_id")val ownerId: Int?,
+    @SerializedName("organization_id")val organizationId: Int?,
     val brand: String,
     val model: String,
     val year: Int,
-    val engineCapacity: Int,
-    val enginePower: Int,
+    @SerializedName("engine_capacity")val engineCapacity: Int,
+    @SerializedName("engine_power")val enginePower: Int,
     val plates: String?,
-    val expectedFuel: Double
+    @SerializedName("expected_fuel")val expectedFuel: Double
 )
 
