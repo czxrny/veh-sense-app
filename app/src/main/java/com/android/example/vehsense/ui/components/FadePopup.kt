d0e1b407a8210dcb85c7622d8b21d3c25f4888cd
czxrny <sczarnecki091@gmail.com>
2026-01-22 20:35:25 +0100

    fix: fade popup is centered

--- app/src/main/java/com/android/example/vehsense/ui/components/FadePopup.kt
+++ app/src/main/java/com/android/example/vehsense/ui/components/FadePopup.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.delay
 
@@ -42,7 +43,8 @@ fun FadePopup(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
+                .background(MaterialTheme.colorScheme.background),
+            contentAlignment = Alignment.Center
         ) {
             content()
         }
